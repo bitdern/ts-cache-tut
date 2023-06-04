81c22661e6c9f27ea9fe0d127c8b55e6d4499233
aidan.newell <acnewell09@gmail.com>
2023-06-04 08:05:16 -0500

    responsive links inside navbar

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 export default function NavBar() {
   return (
@@ -13,6 +13,12 @@ export default function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/">ACN</Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-navbar" />
+        <Navbar.Collapse id="main-navbar">
+          <Nav>
+            <Nav.Link href="/hello">Hello</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
