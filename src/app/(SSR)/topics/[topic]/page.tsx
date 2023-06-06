bc4473eedec4319c76430b7d2a453899c25bad90
aidan.newell <acnewell09@gmail.com>
2023-06-06 07:16:16 -0500

    getting started on dynamic route paths

---
+++ src/app/(SSR)/topics/[topic]/page.tsx
@@ -0,0 +1,8 @@
+interface PageProps {
+  params: { topic: string };
+  // searchParams: { [key: string]: string | string[] | undefined},
+}
+
+export default async function Page({ params: { topic } }: PageProps) {
+  return <div>{topic}</div>;
+}
