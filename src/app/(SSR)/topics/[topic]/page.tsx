e902b097f1c16a634fcad9c68e20621c0da80df0
aidan.newell <acnewell09@gmail.com>
2023-06-07 07:35:48 -0500

    query-based renedering page

--- src/app/(SSR)/topics/[topic]/page.tsx
+++ src/app/(SSR)/topics/[topic]/page.tsx
@@ -1,15 +1,37 @@
 import { UnsplashImage } from "@/models/unsplash-image";
+import Image from "next/image";
+import styles from "./TopicPage.module.css";
 
 interface PageProps {
   params: { topic: string };
   // searchParams: { [key: string]: string | string[] | undefined},
 }
 
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return ["health", "fitness", "coding"].map((topic) => ({ topic }));
+}
+
 export default async function Page({ params: { topic } }: PageProps) {
   const response = await fetch(
     `https://api.unsplash.com/photos/random?query=${topic}&count=30&client_id=${process.env.UNSPLASH_ACCESS_KEY}`
   );
-  const images: UnsplashImage;
+  const images: UnsplashImage[] = await response.json();
 
-  return <div>{topic}</div>;
+  return (
+    <div>
+      <h1>{topic}</h1>
+      {images.map((image) => (
+        <Image
+          src={image.urls.raw}
+          width={250}
+          height={250}
+          alt={image.description}
+          key={image.urls.raw}
+          className={styles.image}
+        />
+      ))}
+    </div>
+  );
 }
