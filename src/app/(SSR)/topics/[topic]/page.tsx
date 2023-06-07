0ac8eb412310f5a2717187aeb8c45e42126f242a
aidan.newell <acnewell09@gmail.com>
2023-06-07 07:53:42 -0500

    added an alert descriptor to the page

--- src/app/(SSR)/topics/[topic]/page.tsx
+++ src/app/(SSR)/topics/[topic]/page.tsx
@@ -1,13 +1,16 @@
 import { UnsplashImage } from "@/models/unsplash-image";
 import Image from "next/image";
 import styles from "./TopicPage.module.css";
+import { Alert } from "@/components/bootstrap";
 
 interface PageProps {
   params: { topic: string };
   // searchParams: { [key: string]: string | string[] | undefined},
 }
 
-export const dynamicParams = false;
+// export const revalidate = 0;
+
+// export const dynamicParams = false;
 
 export function generateStaticParams() {
   return ["health", "fitness", "coding"].map((topic) => ({ topic }));
@@ -21,6 +24,13 @@ export default async function Page({ params: { topic } }: PageProps) {
 
   return (
     <div>
+      <Alert>
+        This page uses <strong>generateStaticParams</strong> to render and cache
+        static pages at build time, even though the URL has a dynamic parameter.
+        Pages that are not included in generateStaticParams will be fetched &
+        rendered on first access and then{" "}
+        <strong>cached for subsequent requests</strong> (this can be disabled).
+      </Alert>
       <h1>{topic}</h1>
       {images.map((image) => (
         <Image
