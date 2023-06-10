24a323703c2a2483d3d2b4b8409a0e74b79fbcf1
aidan.newell <acnewell09@gmail.com>
2023-06-10 07:56:14 -0500

    building the unsplash image search API route

--- src/app/(CSR)/search/SearchPage.tsx
+++ src/app/(CSR)/search/SearchPage.tsx
@@ -6,6 +6,12 @@ import { Button, Form } from "react-bootstrap";
 export default function SearchPage() {
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const query = formData.get("query")?.toString().trim();
+
+    if (query) {
+      alert(process.env.UNSPLASH_ACCESS_KEY);
+    }
   }
 
   return (
---
+++ src/app/api/search/route.tsx
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const query = searchParams.get("query");
+
+  if (!query) {
+    return NextResponse.json({ error: "No query provided" }, { status: 400 });
+  }
+
+  const response = await fetch(
+    `https://api.unsplash.com/search/photos?query=${query}`
+  );
+}
